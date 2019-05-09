0e6f82ffee19fc8a19640819584a3ca04f214b63
Sagar Bhatt <sagarkbhatt95@gmail.com>
2019-05-09 17:49:47 +0530

    Add docker-compose for db connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 app.set("port", process.env.PORT || 8080);
 
-const server = app.listen(app.get("port"), () => {
-  console.log("Express server listening on port " + server.address().port);
+app.listen(app.get("port"), () => {
+  console.log("Express server started and running!");
 });
