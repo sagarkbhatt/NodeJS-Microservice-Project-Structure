15e82d9afef657d07d692f221c5abe50eed9e41b
Sagar Bhatt <sagarkbhatt95@gmail.com>
2019-05-10 15:04:36 +0530

    Add body-parser middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const BodyParser = require('body-parser');
 const BookRoutes = require("./routes/BookRoutes");
+
+// parse application/json
+app.use(BodyParser.json())
 
 app.use("/api/book", BookRoutes);
 
