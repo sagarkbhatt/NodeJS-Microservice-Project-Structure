da0f2b21602d8fb2439df4374d76f7631406b2f7
Sagar Bhatt <sagarkbhatt95@gmail.com>
2019-05-10 15:06:50 +0530

    Format code

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
-const BodyParser = require('body-parser');
+const BodyParser = require("body-parser");
 const BookRoutes = require("./routes/BookRoutes");
 
 // parse application/json
-app.use(BodyParser.json())
+app.use(BodyParser.json());
 
 app.use("/api/book", BookRoutes);
 
